0b80de0d9ca98d1328b54aa91c7f34bf92373df7
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-13 20:47:21 +0000

    first commit

---
+++ lib/washington_ski_report.rb
@@ -0,0 +1,6 @@
+require "washington_ski_report/version"
+
+module WashingtonSkiReport
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/washington_ski_report/version.rb
@@ -0,0 +1,3 @@
+module WashingtonSkiReport
+  VERSION = "0.1.0"
+end
