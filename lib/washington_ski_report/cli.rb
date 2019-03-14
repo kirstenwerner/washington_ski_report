25c237cec2e79c3edf458812a462435cf473e9d5
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-14 22:14:41 +0000

    work in cli.rb

--- lib/washington_ski_report/cli.rb
+++ lib/washington_ski_report/cli.rb
@@ -2,6 +2,20 @@ class WashingtonSkiReport::CLI
   
   def call 
     puts "Welcome to your Washington ski resort report!"
+    list_resorts
+    menu 
+  end 
+  
+  def list_resorts
+    puts " 
+      1. Mt. Baker - open 
+      2. Alpental - open 
+      3. Crystal - open
+      "
+  end 
+  
+  def menu 
+    puts "Select the resort you'd like more information about."
   end 
   
 end
