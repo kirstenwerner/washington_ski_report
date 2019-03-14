0bff451e155b5a3d17b77b25486f94cd3a16ab93
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-14 22:52:26 +0000

    work in cli.rb

--- lib/washington_ski_report/cli.rb
+++ lib/washington_ski_report/cli.rb
@@ -1,12 +1,12 @@
 class WashingtonSkiReport::CLI 
   
   def call 
-    puts "Welcome to your Washington ski resort report!"
     list_resorts
     menu 
   end 
   
   def list_resorts
+    puts "Welcome to your Washington ski resort report! \n"
     puts " 
       1. Mt. Baker - open 
       2. Alpental - open 
@@ -15,7 +15,18 @@ class WashingtonSkiReport::CLI
   end 
   
   def menu 
-    puts "Select the resort you'd like more information about."
+    puts "Select the resort you'd like more information about, or enter 'exit' to exit."
+    input = gets.strip
+    while input != "exit"
+      case input 
+      when '1' 
+        puts ""
+      when '2'
+        puts '' 
+      when '3'
+        puts '' 
+      end 
+    end 
   end 
   
 end
