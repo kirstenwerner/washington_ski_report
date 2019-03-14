09aa901832fd55866dab56bc93e0fc7845c228c3
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-14 21:33:55 +0000

    set up envirnment

--- lib/washington_ski_report.rb
+++ lib/washington_ski_report.rb
@@ -1,6 +1,8 @@
-require "washington_ski_report/version"
+require_relative "washington_ski_report/version"
 
 module WashingtonSkiReport
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require_relative './washington_ski_report/cli'
---
+++ lib/washington_ski_report/cli.rb
@@ -0,0 +1,7 @@
+class WashingtonSkiReport::CLI 
+  
+  def call 
+    puts "Welcome to your Washington ski resort report!"
+  end 
+  
+end
