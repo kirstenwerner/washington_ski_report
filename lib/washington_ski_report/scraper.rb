279ba6d30ea9ca0d3b56f4f7fa63efb4281da81d
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-15 05:25:59 +0000

    work on #scraper

--- lib/washington_ski_report/scraper.rb
+++ lib/washington_ski_report/scraper.rb
@@ -1,9 +1,13 @@
 class WashingtonSkiReport::Scraper
   
   def scrape 
-    doc = Nokogiri::HTML(open('http://skiwashington.com/resorts/'))
+    doc = Nokogiri::HTML(open('http://www.skibum.net/west-coast/washington-ski-areas/'))
     doc
     binding.pry 
+    
+    # name = doc.css('p').css('b').css('a').first.text
+    # url = doc.css('p').css('b').css('a').first['href']
+    # location_and_stats = doc.css('p').css('b').first.children.last.text
   end 
   
 end 
