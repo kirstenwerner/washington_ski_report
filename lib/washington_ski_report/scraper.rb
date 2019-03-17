7dfaf776a417624c658b7c7655499dfb8bf62add
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-17 16:10:28 +0000

    work on #snow_scrape

--- lib/washington_ski_report/scraper.rb
+++ lib/washington_ski_report/scraper.rb
@@ -32,7 +32,21 @@ class WashingtonSkiReport::Scraper
     name_array = resort_names.map {|resort| resort.text if resort != nil} - [nil]
     url_array = resort_names.map {|resort| resort['href'] if resort != nil} - [nil]
     
-    url_array.map do |url|
+    url_array.each do |url| 
+      @doc_url = Nokogiri::HTML(open('http://www.skicentral.com/' + url))
+      @new_snow = @doc_url.css('table').css('td')[13].text
+      @new_snow_detail = @doc_url.css('table').css('td')[15].text
+      @season_total = @doc_url.css('table').css('td')[17].text
+      @surface_conditions = @doc_url.css('table').css('td')[9].text
+      @secondary_surface_conditions = @doc_url.css('table').css('td')[11].text
+      @base_depth = @doc_url.css('table').css('td')[4].text
+      @runs_open = @@doc_url.css('table').css('td')[5].text.gsub(" ", '')
+      @lifts_open = @doc_url.css('table').css('td')[6].text
+      @percent_open = @doc_url.css('table').css('td')[7].text
+      @forcast_weather = @doc_url.css('table').css('td')[27].text
+      @forcast_base_temp = @doc_url.css('table').css('td')[29].text
+      @forcast_summit_temp = @doc_url.css('table').css('td')[31].text
+      
     end 
     
     # snowfall = doc.css('div').css('table').map.with_index {|name, i| name if i%2 != 0} - [nil]
