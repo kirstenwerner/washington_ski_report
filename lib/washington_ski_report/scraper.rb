095c40bf7025312abd1422c6bc03b022a9685d76
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-17 16:32:40 +0000

    work on @snow_scrape

--- lib/washington_ski_report/scraper.rb
+++ lib/washington_ski_report/scraper.rb
@@ -32,26 +32,29 @@ class WashingtonSkiReport::Scraper
     name_array = resort_names.map {|resort| resort.text if resort != nil} - [nil]
     url_array = resort_names.map {|resort| resort['href'] if resort != nil} - [nil]
     
-    url_array.each do |url| 
-      @doc_url = Nokogiri::HTML(open('http://www.skicentral.com/' + url))
-      @new_snow = @doc_url.css('table').css('td')[13].text
-      @new_snow_detail = @doc_url.css('table').css('td')[15].text
-      @season_total = @doc_url.css('table').css('td')[17].text
-      @surface_conditions = @doc_url.css('table').css('td')[9].text
-      @secondary_surface_conditions = @doc_url.css('table').css('td')[11].text
-      @base_depth = @doc_url.css('table').css('td')[4].text
-      @runs_open = @@doc_url.css('table').css('td')[5].text.gsub(" ", '')
-      @lifts_open = @doc_url.css('table').css('td')[6].text
-      @percent_open = @doc_url.css('table').css('td')[7].text
-      @forcast_weather = @doc_url.css('table').css('td')[27].text
-      @forcast_base_temp = @doc_url.css('table').css('td')[29].text
-      @forcast_summit_temp = @doc_url.css('table').css('td')[31].text
-      
-    end 
+    @i = 0
+    @@gnar_details = []
     
-    # snowfall = doc.css('div').css('table').map.with_index {|name, i| name if i%2 != 0} - [nil]
-    # snowfall_array = snowfall.map {|resort| resort.css('td')[3].text}
+    url_array.map do |url| 
+      doc_url = Nokogiri::HTML(open('http://www.skicentral.com/' + url))
+      
+      new_snow = doc_url.css('table').css('td')[13].text
+      new_snow_detail = doc_url.css('table').css('td')[15].text
+      season_total = doc_url.css('table').css('td')[17].text
+      surface_conditions = doc_url.css('table').css('td')[9].text
+      secondary_surface_conditions = doc_url.css('table').css('td')[11].text
+      base_depth = doc_url.css('table').css('td')[4].text
+      runs_open = doc_url.css('table').css('td')[5].text.gsub(" ", '').gsub("\n", " ")
+      lifts_open = doc_url.css('table').css('td')[6].text.gsub(" ", '').gsub("\n", " ")
+      percent_open = doc_url.css('table').css('td')[7].text
+      forcast_weather = doc_url.css('table').css('td')[27].text
+      forcast_base_temp = doc_url.css('table').css('td')[29].text
+      forcast_summit_temp = doc_url.css('table').css('td')[31].text
+      
+      @@gnar_details << {:name => name_array[@i], :new_snow => new_snow, :new_snow_detail => new_snow_detail, :season_total => season_total, :surface_conditions => surface_conditions, :secondary_surface_conditions => secondary_surface_conditions, :base_depth => base_depth, :runs_open => runs_open, :lifts_open => lifts_open, :percent_open => percent_open, :forcast_weather => forcast_weather, :forcast_base_temp => forcast_base_temp, :forcast_summit_temp => forcast_summit_temp}
+      
+      @i += 1 
+    end 
     binding.pry
   end 
-  
 end 
