aee5ded097efda6ea64e541c2f5bae2bb26b8abd
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-20 18:02:39 +0000

    work on #scrape_snow

--- lib/washington_ski_report/scraper.rb
+++ lib/washington_ski_report/scraper.rb
@@ -30,6 +30,7 @@ class WashingtonSkiReport::Scraper
     
     resort_names = doc.css('div').css('a')[10..29].map.with_index {|name, i| name if i%2 == 0}
     name_array = resort_names.map {|resort| resort.text if resort != nil} - [nil]
+    corrected_name_array = name_array.map {|name| name.gsub("Mount", "Mt.").gsub("Pas", "Pass").gsub("Mt.ain", "Mountain")}
     url_array = resort_names.map {|resort| resort['href'] if resort != nil} - [nil]
     
     @i = 0
@@ -38,20 +39,36 @@ class WashingtonSkiReport::Scraper
     url_array.map do |url| 
       doc_url = Nokogiri::HTML(open('http://www.skicentral.com/' + url))
       
-      new_snow = doc_url.css('table').css('td')[13].text
-      new_snow_detail = doc_url.css('table').css('td')[15].text.gsub("   ", " - ").split("\"").map {|str| str + "\""}.join(", ")
-      season_total = doc_url.css('table').css('td')[17].text
-      surface_conditions = doc_url.css('table').css('td')[9].text
-      secondary_surface_conditions = doc_url.css('table').css('td')[11].text
-      base_depth = doc_url.css('table').css('td')[4].text
-      runs_open = doc_url.css('table').css('td')[5].text.gsub("\n", "").gsub('to', 't o').gsub('of', 'of ').gsub(/[ ]{2,}/, " ")
-      lifts_open = doc_url.css('table').css('td')[6].text.gsub("\n", "").gsub('to', 't o').gsub('of', 'of ').gsub(/[ ]{2,}/, " ")
-      percent_open = doc_url.css('table').css('td')[7].text
-      forcast_weather = doc_url.css('table').css('td')[27].text
-      forcast_base_temp = doc_url.css('table').css('td')[29].text
-      forcast_summit_temp = doc_url.css('table').css('td')[31].text
-      
-      @@gnar_details << {:name => name_array[@i], :new_snow => new_snow, :new_snow_detail => new_snow_detail, :season_total => season_total, :surface_conditions => surface_conditions, :secondary_surface_conditions => secondary_surface_conditions, :base_depth => base_depth, :runs_open => runs_open, :lifts_open => lifts_open, :percent_open => percent_open, :forcast_weather => forcast_weather, :forcast_base_temp => forcast_base_temp, :forcast_summit_temp => forcast_summit_temp}
+      doc_url.css('table').css('td').each.with_index do |object, i|
+         if object.text == "New Snow:"
+           @new_snow = doc_url.css('table').css('td')[i + 1].text
+         elsif object.text == "7 Day Details"
+           @new_snow_detail = doc_url.css('table').css('td')[i + 1].text.gsub("   ", " - ").split("\"").map {|str| str + "\""}.join(", ")
+         elsif object.text == "Season Total"
+           @season_total = doc_url.css('table').css('td')[i + 1].text
+         elsif object.text == "Primary Surface Conditions:"
+           @surface_conditions = doc_url.css('table').css('td')[i + 1].text
+         elsif object.text == "Secondary Surface Conditions:"
+           @secondary_surface_conditions = doc_url.css('table').css('td')[i + 1].text
+         elsif object.text == "Base Depth"
+           @base_depth = doc_url.css('table').css('td')[i + 4].text
+         elsif object.text == "Runs Open"
+          @runs_open = doc_url.css('table').css('td')[i + 4].text.gsub("\n", "").gsub('to', 't o').gsub('of', 'of ').gsub(/[ ]{2,}/, " ")
+         elsif object.text == "Lifts Open"
+           @lifts_open = doc_url.css('table').css('td')[i + 4].text.gsub("\n", "").gsub('to', 't o').gsub('of', 'of ').gsub(/[ ]{2,}/, " ")
+         elsif object.text == "Percent Open"
+           @percent_open = doc_url.css('table').css('td')[i + 4].text
+         elsif object.text == "Forecast Weather"
+           @forcast_weather = doc_url.css('table').css('td')[i + 1].text
+         elsif object.text == "Forecast Base Temperature"
+           @forcast_base_temp = doc_url.css('table').css('td')[i + 1].text
+         elsif object.text == "Forecast Summit Temperature"
+           @forcast_summit_temp = doc_url.css('table').css('td')[i + 1].text
+         end 
+       end 
+       binding.pry
+
+      @@gnar_details << {:name => name_array[@i], :new_snow => @new_snow, :new_snow_detail => @new_snow_detail, :season_total => @season_total, :surface_conditions => @surface_conditions, :secondary_surface_conditions => @secondary_surface_conditions, :base_depth => @base_depth, :runs_open => @runs_open, :lifts_open => @lifts_open, :percent_open => @percent_open, :forcast_weather => @forcast_weather, :forcast_base_temp => @forcast_base_temp, :forcast_summit_temp => @forcast_summit_temp}
       @i += 1 
     end 
     @@gnar_details
